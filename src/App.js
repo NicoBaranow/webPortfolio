4a11d38072e0c3cbe8b2fe86e1d19510c26c1e45
Agustin Baranowski <baranowskiagustin@gmail.com>
2023-01-12 13:36:31 -0300

    about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Header from './components/header'
 import Home from './components/home'
+import About from './components/about'
 import './styles/app.css'
 
-
 function App() {
+
+  const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry=>{
+      console.log(entry)
+      if(entry.isIntersecting) entry.target.classList.toggle('show', entry.isIntersecting);
+      else entry.target.classList.remove('show')
+    })
+  })
+
+  const hiddenElements = document.querySelectorAll('.hidden')
+  hiddenElements.forEach(el=>observer.observe(el))
+
   return (
     <div>
       <Header/>
       <Home/>
-
+      <section className='hidden'>
+        <About/>
+      </section>
     </div>
   )
 }
---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/about.css'
+
+export default function Header(){
+    
+    return(
+        <div className="about">
+            <div className="">
+                <h1 className="title">About me</h1>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, veritatis. Perferendis incidunt quasi nemo sint nulla quos architecto pariatur ratione mollitia molestias ut sit eveniet quam, expedita eaque vel commodi.</p>
+            </div>
+        </div>
+    )
+}
